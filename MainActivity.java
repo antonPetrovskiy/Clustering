4f5860660e11b4679cd56d80f4cb89e75c0d25a0
antonPetrovskiy <toschapetrovski@gmail.com>
2018-01-04 14:08:29 +0200

    added centres of clusters

--- MainActivity.java
+++ MainActivity.java
@@ -116,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        if(Integer.parseInt(x17.getText().toString())<=1){
+        if(Integer.parseInt(x17.getText().toString())<=0){
             x1sum+=x1;
             y1sum+=y1;
             all1+=1;
@@ -125,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
             y2sum+=y1;
             all2+=1;
         }
-        if(Integer.parseInt(x18.getText().toString())<=1){
+        if(Integer.parseInt(x18.getText().toString())<=0){
             x1sum+=x2;
             y1sum+=y2;
             all1+=1;
@@ -134,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
             y2sum+=y2;
             all2+=1;
         }
-        if(Integer.parseInt(x19.getText().toString())<=1){
+        if(Integer.parseInt(x19.getText().toString())<=0){
             x1sum+=x3;
             y1sum+=y3;
             all1+=1;
@@ -143,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
             y2sum+=y3;
             all2+=1;
         }
-        if(Integer.parseInt(x20.getText().toString())<=1){
+        if(Integer.parseInt(x20.getText().toString())<=0){
             x1sum+=x4;
             y1sum+=y4;
             all1+=1;
@@ -152,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
             y2sum+=y4;
             all2+=1;
         }
-        if(Integer.parseInt(x21.getText().toString())<=1){
+        if(Integer.parseInt(x21.getText().toString())<=0){
             x1sum+=x5;
             y1sum+=y5;
             all1+=1;
@@ -161,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
             y2sum+=y5;
             all2+=1;
         }
-        if(Integer.parseInt(x22.getText().toString())<=1){
+        if(Integer.parseInt(x22.getText().toString())<=0){
             x1sum+=x6;
             y1sum+=y6;
             all1+=1;
@@ -170,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
             y2sum+=y6;
             all2+=1;
         }
-        if(Integer.parseInt(x23.getText().toString())<=1){
+        if(Integer.parseInt(x23.getText().toString())<=0){
             x1sum+=x7;
             y1sum+=y7;
             all1+=1;
@@ -179,7 +179,7 @@ public class MainActivity extends AppCompatActivity {
             y2sum+=y7;
             all2+=1;
         }
-        if(Integer.parseInt(x24.getText().toString())<=1){
+        if(Integer.parseInt(x24.getText().toString())<=0){
             x1sum+=x8;
             y1sum+=y8;
             all1+=1;
@@ -200,8 +200,8 @@ public class MainActivity extends AppCompatActivity {
 
         EditText centreX1 = (EditText)findViewById(R.id.editText97);
         EditText centreY1 = (EditText)findViewById(R.id.editText98);
-        centreX1.setText(Double.toString(centr_x1));
-        centreY1.setText(Double.toString(centr_y1));
+        centreX1.setText(Double.toString(centr_x1)+" ; "+Double.toString(centr_y1));
+        centreY1.setText(Double.toString(centr_x2)+" ; "+Double.toString(centr_y2));
     }
 
     public void iteration_1(){
@@ -261,89 +261,89 @@ public class MainActivity extends AppCompatActivity {
         double x2sum = 0;
         double y2sum = 0;
         if(Double.parseDouble(x25.getText().toString()) < Double.parseDouble(x33.getText().toString())){
-            x41.setText("1");
+            x41.setText("0");
             x1sum+=x1;
             y1sum+=y1;
             all1+=1;
         }else{
-            x41.setText("2");
+            x41.setText("1");
             x2sum+=x1;
             y2sum+=y1;
             all2+=1;
         }
         if(Double.parseDouble(x26.getText().toString()) < Double.parseDouble(x34.getText().toString())){
-            x42.setText("1");
+            x42.setText("0");
             x1sum+=x2;
             y1sum+=y2;
             all1+=1;
         }else{
-            x42.setText("2");
+            x42.setText("1");
             x2sum+=x2;
             y2sum+=y2;
             all2+=1;
         }
         if(Double.parseDouble(x27.getText().toString()) < Double.parseDouble(x35.getText().toString())){
-            x43.setText("1");
+            x43.setText("0");
             x1sum+=x3;
             y1sum+=y3;
             all1+=1;
         }else{
-            x43.setText("2");
+            x43.setText("1");
             x2sum+=x3;
             y2sum+=y3;
             all2+=1;
         }
         if(Double.parseDouble(x28.getText().toString()) < Double.parseDouble(x36.getText().toString())){
-            x44.setText("1");
+            x44.setText("0");
             x1sum+=x4;
             y1sum+=y4;
             all1+=1;
         }else{
-            x44.setText("2");
+            x44.setText("1");
             x2sum+=x4;
             y2sum+=y4;
             all2+=1;
         }
         if(Double.parseDouble(x29.getText().toString()) < Double.parseDouble(x37.getText().toString())){
-            x45.setText("1");
+            x45.setText("0");
             x1sum+=x5;
             y1sum+=y5;
             all1+=1;
         }else{
-            x45.setText("2");
+            x45.setText("1");
             x2sum+=x5;
             y2sum+=y5;
             all2+=1;
         }
         if(Double.parseDouble(x30.getText().toString()) < Double.parseDouble(x38.getText().toString())){
-            x46.setText("1");
+            x46.setText("0");
             x1sum+=x6;
             y1sum+=y6;
             all1+=1;
         }else{
-            x46.setText("2");
+            x46.setText("1");
             x2sum+=x6;
             y2sum+=y6;
             all2+=1;
         }
         if(Double.parseDouble(x31.getText().toString()) < Double.parseDouble(x39.getText().toString())){
-            x47.setText("1");
+            x47.setText("0");
             x1sum+=x7;
             y1sum+=y7;
             all1+=1;
         }else{
-            x47.setText("2");
+            x47.setText("1");
             x2sum+=x7;
             y2sum+=y7;
             all2+=1;
         }
         if(Double.parseDouble(x32.getText().toString()) < Double.parseDouble(x40.getText().toString())){
-            x48.setText("1");
+            x48.setText("0");
             x1sum+=x8;
             y1sum+=y8;
             all1+=1;
         }else{
-            x48.setText("2");
+            x48.setText("1");
             x2sum+=x8;
             y2sum+=y8;
             all2+=1;
@@ -354,6 +354,11 @@ public class MainActivity extends AppCompatActivity {
         centr_y1 = y1sum/all1;
         centr_y2 = y2sum/all2;
 
+
+        EditText centreX1 = (EditText)findViewById(R.id.editText99);
+        EditText centreY1 = (EditText)findViewById(R.id.editText100);
+        centreX1.setText(Double.toString(centr_x1)+" ; "+Double.toString(centr_y1));
+        centreY1.setText(Double.toString(centr_x2)+" ; "+Double.toString(centr_y2));
 
     }
     public void iteration_2(){
@@ -410,89 +415,89 @@ public class MainActivity extends AppCompatActivity {
         double x2sum = 0;
         double y2sum = 0;
         if(Double.parseDouble(x49.getText().toString()) < Double.parseDouble(x57.getText().toString())){
-            x65.setText("1");
+            x65.setText("0");
             x1sum+=x1;
             y1sum+=y1;
             all1+=1;
         }else{
-            x65.setText("2");
+            x65.setText("1");
             x2sum+=x1;
             y2sum+=y1;
             all2+=1;
         }
         if(Double.parseDouble(x50.getText().toString()) < Double.parseDouble(x58.getText().toString())){
-            x66.setText("1");
+            x66.setText("0");
             x1sum+=x2;
             y1sum+=y2;
             all1+=1;
         }else{
-            x66.setText("2");
+            x66.setText("1");
             x2sum+=x2;
             y2sum+=y2;
             all2+=1;
         }
         if(Double.parseDouble(x51.getText().toString()) < Double.parseDouble(x59.getText().toString())){
-            x67.setText("1");
+            x67.setText("0");
             x1sum+=x3;
             y1sum+=y3;
             all1+=1;
         }else{
-            x67.setText("2");
+            x67.setText("1");
             x2sum+=x3;
             y2sum+=y3;
             all2+=1;
         }
         if(Double.parseDouble(x52.getText().toString()) < Double.parseDouble(x60.getText().toString())){
-            x68.setText("1");
+            x68.setText("0");
             x1sum+=x4;
             y1sum+=y4;
             all1+=1;
         }else{
-            x68.setText("2");
+            x68.setText("1");
             x2sum+=x4;
             y2sum+=y4;
             all2+=1;
         }
         if(Double.parseDouble(x53.getText().toString()) < Double.parseDouble(x61.getText().toString())){
-            x69.setText("1");
+            x69.setText("0");
             x1sum+=x5;
             y1sum+=y5;
             all1+=1;
         }else{
-            x69.setText("2");
+            x69.setText("1");
             x2sum+=x5;
             y2sum+=y5;
             all2+=1;
         }
         if(Double.parseDouble(x54.getText().toString()) < Double.parseDouble(x62.getText().toString())){
-            x70.setText("1");
+            x70.setText("0");
             x1sum+=x6;
             y1sum+=y6;
             all1+=1;
         }else{
-            x70.setText("2");
+            x70.setText("1");
             x2sum+=x6;
             y2sum+=y6;
             all2+=1;
         }
         if(Double.parseDouble(x55.getText().toString()) < Double.parseDouble(x63.getText().toString())){
-            x71.setText("1");
+            x71.setText("0");
             x1sum+=x7;
             y1sum+=y7;
             all1+=1;
         }else{
-            x71.setText("2");
+            x71.setText("1");
             x2sum+=x7;
             y2sum+=y7;
             all2+=1;
         }
         if(Double.parseDouble(x56.getText().toString()) < Double.parseDouble(x64.getText().toString())){
-            x72.setText("1");
+            x72.setText("0");
             x1sum+=x8;
             y1sum+=y8;
             all1+=1;
         }else{
-            x72.setText("2");
+            x72.setText("1");
             x2sum+=x8;
             y2sum+=y8;
             all2+=1;
@@ -502,6 +507,11 @@ public class MainActivity extends AppCompatActivity {
         centr_x2 = x2sum/all2;
         centr_y1 = y1sum/all1;
         centr_y2 = y2sum/all2;
+
+        EditText centreX1 = (EditText)findViewById(R.id.editText101);
+        EditText centreY1 = (EditText)findViewById(R.id.editText102);
+        centreX1.setText(Double.toString(centr_x1)+" ; "+Double.toString(centr_y1));
+        centreY1.setText(Double.toString(centr_x2)+" ; "+Double.toString(centr_y2));
 
     }
     public void iteration_3(){
@@ -558,89 +568,89 @@ public class MainActivity extends AppCompatActivity {
         double x2sum = 0;
         double y2sum = 0;
         if(Double.parseDouble(x73.getText().toString()) < Double.parseDouble(x81.getText().toString())){
-            x89.setText("1");
+            x89.setText("0");
             x1sum+=x1;
             y1sum+=y1;
             all1+=1;
         }else{
-            x89.setText("2");
+            x89.setText("1");
             x2sum+=x1;
             y2sum+=y1;
             all2+=1;
         }
         if(Double.parseDouble(x74.getText().toString()) < Double.parseDouble(x82.getText().toString())){
-            x90.setText("1");
+            x90.setText("0");
             x1sum+=x2;
             y1sum+=y2;
             all1+=1;
         }else{
-            x90.setText("2");
+            x90.setText("1");
             x2sum+=x2;
             y2sum+=y2;
             all2+=1;
         }
         if(Double.parseDouble(x75.getText().toString()) < Double.parseDouble(x83.getText().toString())){
-            x91.setText("1");
+            x91.setText("0");
             x1sum+=x3;
             y1sum+=y3;
             all1+=1;
         }else{
-            x91.setText("2");
+            x91.setText("1");
             x2sum+=x3;
             y2sum+=y3;
             all2+=1;
         }
         if(Double.parseDouble(x76.getText().toString()) < Double.parseDouble(x84.getText().toString())){
-            x92.setText("1");
+            x92.setText("0");
             x1sum+=x4;
             y1sum+=y4;
             all1+=1;
         }else{
-            x92.setText("2");
+            x92.setText("1");
             x2sum+=x4;
             y2sum+=y4;
             all2+=1;
         }
         if(Double.parseDouble(x77.getText().toString()) < Double.parseDouble(x85.getText().toString())){
-            x93.setText("1");
+            x93.setText("0");
             x1sum+=x5;
             y1sum+=y5;
             all1+=1;
         }else{
-            x93.setText("2");
+            x93.setText("1");
             x2sum+=x5;
             y2sum+=y5;
             all2+=1;
         }
         if(Double.parseDouble(x78.getText().toString()) < Double.parseDouble(x86.getText().toString())){
-            x94.setText("1");
+            x94.setText("0");
             x1sum+=x6;
             y1sum+=y6;
             all1+=1;
         }else{
-            x94.setText("2");
+            x94.setText("1");
             x2sum+=x6;
             y2sum+=y6;
             all2+=1;
         }
         if(Double.parseDouble(x79.getText().toString()) < Double.parseDouble(x87.getText().toString())){
-            x95.setText("1");
+            x95.setText("0");
             x1sum+=x7;
             y1sum+=y7;
             all1+=1;
         }else{
-            x95.setText("2");
+            x95.setText("1");
             x2sum+=x7;
             y2sum+=y7;
             all2+=1;
         }
         if(Double.parseDouble(x80.getText().toString()) < Double.parseDouble(x88.getText().toString())){
-            x96.setText("1");
+            x96.setText("0");
             x1sum+=x8;
             y1sum+=y8;
             all1+=1;
         }else{
-            x96.setText("2");
+            x96.setText("1");
             x2sum+=x8;
             y2sum+=y8;
             all2+=1;
@@ -650,6 +660,11 @@ public class MainActivity extends AppCompatActivity {
         centr_x2 = x2sum/all2;
         centr_y1 = y1sum/all1;
         centr_y2 = y2sum/all2;
+
+        EditText centreX1 = (EditText)findViewById(R.id.editText103);
+        EditText centreY1 = (EditText)findViewById(R.id.editText104);
+        centreX1.setText(Double.toString(centr_x1)+" ; "+Double.toString(centr_y1));
+        centreY1.setText(Double.toString(centr_x2)+" ; "+Double.toString(centr_y2));
     }
 
 
